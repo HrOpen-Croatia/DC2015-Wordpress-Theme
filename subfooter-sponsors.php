f2f75d9bc7e822843bae9e38edd648b2b8f7f804
Svebor Prstačić <svebor@open.hr>
2015-03-05 10:16:18 +0100

    Replaced site5 logo with new one

--- subfooter-sponsors.php
+++ subfooter-sponsors.php
@@ -6,7 +6,7 @@
         <div class="row">
             <div class="container">
                 
-                <div class="col-sm-6 col-md-6 col-centered"">
+                <div class="col-sm-6 col-md-6 col-centered">
                     <div class="thumbnail">
                         <a href="http://www.open.hr" >
                             <img class="img-responsive" src="http://www.dorscluc.org/wp-content/uploads/2012/12/hropen-logo-mini.png" alt="HrOpen logo" title="Croatian Association for Open Systems and Internet" />
@@ -14,7 +14,7 @@
                     </div>
                 </div>
                 
-                <div class="col-sm-6 col-md6 col-centered"">
+                <div class="col-sm-6 col-md6 col-centered">
                     <div class="thumbnail">
                         <a href="http://www.linux.hr">
                             <img class="img-responsive" src="http://www.dorscluc.org/wp-content/uploads/2012/12/hulk-logo-mini.jpg" alt="HULK logo" title="Croatian Linux Users' Association" />
@@ -36,7 +36,7 @@
                     </div>
                 </div>-->
                 
-                <div class="col-sm-6 col-md-6 col-centered"">
+                <div class="col-sm-6 col-md-6 col-centered">
                     <div class="thumbnail">
                         <a href="http://www.fer.hr/en" ><img class="img-responsive" src="<?php bloginfo('template_url');?>/sponsors/fer.jpg" alt="FER logo" title="Faculty of electrical engineering and computing" /></a>
                     </div>
@@ -68,7 +68,7 @@
                         <a href="http://www.hgk.hr/en" ><img class="silver-sponsor-logo-image" src="<?php bloginfo('template_url');?>/sponsors/hgk-text-logo.png" alt="HGK logo" title="Croatian Chamber of Economy" /></a>
                     </div>
                 </div>-->                
-		<div class="col-sm-6 col-md-3 col-centered"">
+		<div class="col-sm-6 col-md-3 col-centered">
                     <div class="thumbnail">
                         <a href="http://www.pointer.hr/" >
                             <img class="img-responsive" src="<?php bloginfo('template_url');?>/sponsors/pointer-logo.png" />
@@ -84,20 +84,20 @@
         
         <div class="row sponsors row-centered">
             <div class="container">
-                <div class="col-sm-6 col-md-3 col-centered"">
+                <div class="col-sm-6 col-md-3 col-centered">
                     <div class="thumbnail">
                         <a href="http://www.carnet.hr" class="sponsor-logo"><img class="img-responsive" src="<?php bloginfo('template_url');?>/sponsors/carnet.jpg" alt="CARNet logo" title=""/>
                     </div>
                 </div>
-                <div class="col-sm-6 col-md-3 col-centered"">
+                <div class="col-sm-6 col-md-3 col-centered">
                     <div class="thumbnail">
                         <a href="http://www.ultima.hr" class="sponsor-logo"><img class="img-responsive" src="<?php bloginfo('template_url');?>/sponsors/ultima_logo.png" alt="Ultima logo" title=""/>
                         </a>
                     </div>
                 </div>
-                <div class="col-sm-6 col-md-3 col-centered"">
+                <div class="col-sm-6 col-md-3 col-centered">
                     <div class="thumbnail">
-                        <a href="http://www.site5.com/p/croatia-linux-meetup/" ><img class="img-responsive" src="<?php bloginfo('template_url');?>/sponsors/site5-logo.png" alt="Site5 logo" title="Site5 is an international leader in affordable Web hosting solutions for individuals and businesses. For more than a decade, we have provided award-winning customer support and aggressively-priced hosting solutions to customers on six continents. We offer a variety of shared, reseller, cloud, and VPS hosting solutions from nearly two dozen datacenters located in eight different countries."/>
+                        <a href="http://www.site5.com/p/croatia-linux-meetup/" ><img class="img-responsive" src="<?php bloginfo('template_url');?>/sponsors/site5-new-logo.png" alt="Site5 logo" title="Site5 is an international leader in affordable Web hosting solutions for individuals and businesses. For more than a decade, we have provided award-winning customer support and aggressively-priced hosting solutions to customers on six continents. We offer a variety of shared, reseller, cloud, and VPS hosting solutions from nearly two dozen datacenters located in eight different countries."/>
                         </a>
                     </div>
                 </div>
@@ -111,7 +111,7 @@
         
         <div class="row micro-sponsors row-centered">
             <div class="container">
-                <div class="col-sm-6 col-md-2 col-centered"">
+                <div class="col-sm-6 col-md-2 col-centered">
                     <a href="http://https://www.perpetuum.hr/" >
                         <img class="img-responsive" src="<?php bloginfo('template_url');?>/sponsors/perpetuum.png" alt="Perpetuum Mobile logo" title="Perpetuum Mobile" />
                     </a> 
@@ -126,18 +126,18 @@
         
         <div class="row micro-sponsors row-centered">
             <div class="container">
-                <div class="col-sm-6 col-md-2 col-centered"">
+                <div class="col-sm-6 col-md-2 col-centered">
                     <a href="http://www.racunalo.com/" class="sponsor-logo media-sponsor-logo vam">
                         <img class="img-responsive" src="<?php bloginfo('template_url');?>/sponsors/racunalo_com.png" alt="Racunalo.com logo" title="" />
                     </a>
                 </div>
-                <div class="col-sm-6 col-md-2 col-centered"">
+                <div class="col-sm-6 col-md-2 col-centered">
                     <a href="http://www.bug.hr/mreza/" >
                         <img class="img-responsive" src="<?php bloginfo('template_url');?>/sponsors/mreza_logotip-200.png" alt="Mreža logo" title="" />
                     </a>
                 </div>
                 
-                <div class="col-sm-6 col-md-2 col-centered""> 
+                <div class="col-sm-6 col-md-2 col-centered"> 
                     <a href="http://www.linuxzasve.com/" >
                     <img class="img-responsive" src="<?php bloginfo('template_url');?>/sponsors/lzs-250x125.jpg" alt="Linux za sve logo" title="Linux za sve" />
                 </a>
@@ -190,7 +190,7 @@
                     </div>
                 </div>-->
                 
-                <div class="col-sm-6 col-md-6 col-centered"">
+                <div class="col-sm-6 col-md-6 col-centered">
                     <div class="thumbnail">
                         <a href="http://www.fer.hr/en" ><img class="img-responsive" src="<?php bloginfo('template_url');?>/sponsors/fer.jpg" alt="FER logo" title="Faculty of electrical engineering and computing" /></a>
                     </div>
@@ -251,7 +251,7 @@
                 </div>
                 <div class="col-sm-6 col-md-3 col-centered">
                     <div class="thumbnail">
-                        <a href="http://www.site5.com/p/croatia-linux-meetup/" ><img class="img-responsive" src="<?php bloginfo('template_url');?>/sponsors/site5-logo.png" alt="Site5 logo" title="Site5 is an international leader in affordable Web hosting solutions for individuals and businesses. For more than a decade, we have provided award-winning customer support and aggressively-priced hosting solutions to customers on six continents. We offer a variety of shared, reseller, cloud, and VPS hosting solutions from nearly two dozen datacenters located in eight different countries."/>
+                        <a href="http://www.site5.com/p/croatia-linux-meetup/" ><img class="img-responsive" src="<?php bloginfo('template_url');?>/sponsors/site5-new-logo.png" alt="Site5 logo" title="Site5 is an international leader in affordable Web hosting solutions for individuals and businesses. For more than a decade, we have provided award-winning customer support and aggressively-priced hosting solutions to customers on six continents. We offer a variety of shared, reseller, cloud, and VPS hosting solutions from nearly two dozen datacenters located in eight different countries."/>
                         </a>
                     </div>
                 </div>
@@ -285,13 +285,13 @@
                         <img class="img-responsive" src="<?php bloginfo('template_url');?>/sponsors/racunalo_com.png" alt="Racunalo.com logo" title="" />
                     </a>
                 </div>
-                <div class="col-sm-6 col-md-2 col-centered"">
+                <div class="col-sm-6 col-md-2 col-centered">
                     <a href="http://www.bug.hr/mreza/" >
                         <img class="img-responsive" src="<?php bloginfo('template_url');?>/sponsors/mreza_logotip-200.png" alt="Mreža logo" title="" />
                     </a>
                 </div>
                 
-                <div class="col-sm-6 col-md-2 col-centered"">
+                <div class="col-sm-6 col-md-2 col-centered">
                     <a href="http://www.linuxzasve.com/" >
                     <img class="img-responsive" src="<?php bloginfo('template_url');?>/sponsors/lzs-250x125.jpg" alt="Linux za sve logo" title="Linux za sve" />
                 </a>
